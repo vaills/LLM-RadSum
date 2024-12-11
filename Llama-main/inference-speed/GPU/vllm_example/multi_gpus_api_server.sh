81250d46f81b07caa5be3e29de9e586de04a14cf
vaills <97573612+vaills@users.noreply.github.com>
2024-12-11 20:43:03 +0800

    Delete Llama-main directory

--- Llama-main/inference-speed/GPU/vllm_example/multi_gpus_api_server.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python api_server.py \
---model "./Atom-7B-Chat" \
---port 8090 \
---tensor-parallel-size 2
--- Llama-main/inference-speed/GPU/vllm_example/single_gpu_api_server.sh
+++
@@ -1,4 +0,0 @@
-
-CUDA_VISIBLE_DEVICES=0 python api_server.py \
---model "./Atom-7B-Chat" \
---port 8090
